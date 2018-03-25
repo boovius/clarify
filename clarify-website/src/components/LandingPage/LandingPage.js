67aa542ba0642bcc73459bad91ce3e3252314ab3
Joshua Book <joshua.book@gmail.com>
2018-03-25 16:08:18 -0400

    add landing page and the solution components

---
+++ clarify-website/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Container from '../Container/Container';
+import './LandingPage.css';
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="LandingPage">
+        <Container>
+          <h2 className='LandingPage-title'>CLAR<span role='img' aria-label='lightbulb'>💡</span>FY</h2>
+          <div className='LandingPage-tagline'>
+            <h3>Helping stuck people</h3>
+            <h3>take an intentional <span className='LandingPage-tagline--spotlight'>next-step</span></h3>
+            <h3>towards a life of more powerful <span className='LandingPage-tagline--spotlight'>personal meaning</span></h3>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
